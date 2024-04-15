28ed510b39ba40468bfe61eec5bfd3661db77737
janik96v <janik.vollenweider@gmail.com>
2024-04-15 15:07:46 +0200

    File changed

--- runExchangeRateTest.sh
+++ runExchangeRateTest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python train.py --model TENet --window 32 --horizon 5 --highway_window 1 --channel_size 12 --hid1 40 --hid2 10 --data Data/exchange_rate.csv --n_e 8 --A TENet_master/TE/exte.txt --save Model/model.pt 
+python train.py --model TENet --epochs 10 --window 32 --horizon 5 --highway_window 1 --channel_size 12 --hid1 40 --hid2 10 --data Data/exchange_rate.csv --n_e 8 --A TENet_master/TE/exte.txt --save Model/model.pt 
 
 
 

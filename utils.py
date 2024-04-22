a79ea8bb3196feac047d0f3e500fe9ca0ba3c11e
Julia Usher <usher.juliapatricia@gmail.com>
2024-04-22 15:45:53 +0200

    added logging function

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import time
 import os
+import logging
 
 def set_modelID():
     path = 'models/'
@@ -10,3 +11,14 @@ def set_modelID():
     models = [model for model in models if model.startswith(t)]
     i = max([int(model[-1]) for model in models]) + 1
     return f'{t}_{i}'
+
+def logging_setup(args, name):
+    logger = logging.getLogger(name)
+    logging.basicConfig(filename = os.path.join(args.savepath, 'train.log'), level=logging.INFO)
+    logger.info(f'Model ID: {args.modelID}')
+    logger.info(f'Training file: {args.data}, normalisation: {args.normalize}')
+    logger.info(f'Model used: {args.model}')
+    logger.info(f'Number of graph nodes: {args.n_e}')
+    logger.info(f'GNN: channel size {args.channel_size}, layer 1 {args.hid1}, layer 2 {args.hid2}, highway window {args.highway_window}')
+    logger.info(f'CNN: kernel sizes {args.k_size}, window size {args.window}')
+    logger.info(f'Optimiser: {args.optim}, learning rate {args.lr}, gradient clipping {args.clip}')

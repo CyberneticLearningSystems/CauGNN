5151795d9f2a73c943f15659b37c96c386a10f67
Julia Usher <usher.juliapatricia@gmail.com>
2024-05-02 08:20:13 +0200

    moved __init__ to top of file

--- Optim.py
+++ Optim.py
@@ -2,6 +2,17 @@ import math
 import torch.optim as optim
 
 class Optim(object):
+    def __init__(self, params, method, lr, max_grad_norm, lr_decay=1, start_decay_at=None):
+        self.params = list(params)  # careful: params may be a generator
+        self.last_ppl = None
+        self.lr = lr
+        self.max_grad_norm = max_grad_norm
+        self.method = method
+        self.lr_decay = lr_decay
+        self.start_decay_at = start_decay_at
+        self.start_decay = False
+
+        self._makeOptimizer()
 
     def _makeOptimizer(self):
         if self.method == 'sgd':
@@ -14,18 +25,6 @@ class Optim(object):
             self.optimizer = optim.Adam(self.params, lr=self.lr)
         else:
             raise RuntimeError("Invalid optim method: " + self.method)
-
-    def __init__(self, params, method, lr, max_grad_norm, lr_decay=1, start_decay_at=None):
-        self.params = list(params)  # careful: params may be a generator
-        self.last_ppl = None
-        self.lr = lr
-        self.max_grad_norm = max_grad_norm
-        self.method = method
-        self.lr_decay = lr_decay
-        self.start_decay_at = start_decay_at
-        self.start_decay = False
-
-        self._makeOptimizer()
 
     def step(self):
         # Compute gradients norm.

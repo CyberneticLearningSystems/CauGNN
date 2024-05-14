017ae110a068c8b882b241d05b56d7f3dafb9876
janik96v <janik.vollenweider@gmail.com>
2024-05-14 14:21:21 +0200

    File added

---
+++ runATRS_oneAirline.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python main.py --model TENet --window 8 --horizon 4 --batch_size 16 --channel_size 12 --hid1 40 --hid2 10 --epoch 10 --data ./Data/form41_aggregated_monthly_reduced_dataset_southwest.csv --A ./TENet_master/TE/form41_aggregated_quarterly_USC_reduced_TE.txt --form41 True --seed 123 --cuda True

451704a251a5f22f4a2f0fb40c6d855d56e79cf1
janik96v <janik.vollenweider@gmail.com>
2024-05-29 14:13:14 +0200

    Minor changes

--- runATRS.sh
+++ runATRS.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python main.py --model TENet --window 8 --horizon 4 --batch_size 16 --channel_size 12 --hid1 40 --hid2 10 --epochs 10 --data ./Data/form41_aggregated_quarterly_reduced_LargeUSC.csv --A ./TENet_master/TE/form41_aggregated_quarterly_USC_reduced_TE.txt --form41 True --airline_batching True --sharedTE True --seed 123 --cuda '' --print True --tune True
+python main.py --model TENet --window 8 --horizon 4 --batch_size 16 --channel_size 12 --hid1 40 --hid2 10 --epochs 10 --data ./Data/form41_aggregated_quarterly_reduced_LargeUSC.csv --A ./TENet_master/TE/form41_aggregated_quarterly_USC_reduced_TE.txt --form41 True --airline_batching True --sharedTE True --seed 123 --cuda '' --print True --tune ''

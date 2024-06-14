901368895114d93ec3df5ef7471ac166e38f33eb
janik96v <janik.vollenweider@gmail.com>
2024-06-14 19:14:22 +0200

    Output Path changed

--- runTE_calculation.sh
+++ runTE_calculation.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python ./TENet_master/util/Teoriginal.py --datapath ./Data/form41_aggregated_quarterly_pca.csv --outputfolder ./TENet_master/TE/Output --pca_processed True
+python ./TENet_master/util/Teoriginal.py --datapath ./Data/form41_aggregated_quarterly_pca.csv --outputfolder ./TENet_master/TE --pca_processed True

f43995120433f26d145eaae1072a2c313e4e9249
janik96v <janik.vollenweider@gmail.com>
2024-06-18 13:17:53 +0200

    minor changes

--- utils.py
+++ utils.py
@@ -23,6 +23,6 @@ def model_logging(args, path):
         f.write(f'Model used: {args.model}\n')
         f.write(f'Number of graph nodes: {args.n_e}\n')
         f.write(f'GNN: channel size {args.channel_size}, layer 1 {args.hid1}, layer 2 {args.hid2}, highway window {args.highway_window}\n')
-        f.write(f'CNN: kernel sizes {args.k_size}, window size {args.window}\n')
+        f.write(f'CNN: kernel sizes {args.k_size}, window size {args.window}, horizon {args.horizon}, batch size {args.batch_size}, dropout {args.dropout}\n')
         f.write(f'Optimiser: {args.optim}, learning rate {args.lr}, gradient clipping {args.clip}')
         f.close()

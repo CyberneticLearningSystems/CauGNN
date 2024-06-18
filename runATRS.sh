a84f653f4aa5cd32def05b3f0eb282705a2d7218
janik96v <janik.vollenweider@gmail.com>
2024-06-18 13:14:28 +0200

    Parameter tuned

--- runATRS.sh
+++ runATRS.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python main.py --model TENet --window 32 --horizon 4 --batch_size 4 --channel_size 64 --hid1 40 --hid2 20 --lr 0.0019674631267632787 --epochs 100 --data ./Data/form41_aggregated_quarterly_reduced_LargeUSC.csv --A ./TENet_master/TE/form41_aggregated_quarterly_USC_reduced_TE.txt --form41 True --airline_batching True --sharedTE True --seed 123 --cuda '' --print True --tune ''
+python main.py --model TENet --window 32 --horizon 4 --batch_size 20 --channel_size 6 --dropout 0.2 --hid1 20 --hid2 5 --lr 0.00001 --epochs 100 --train 0.6 --data ./Data/form41_aggregated_quarterly_reduced_LargeUSC.csv --A ./TENet_master/TE/form41_aggregated_quarterly_LargeUSC_reduced_TE.txt --form41 True --airline_batching True --sharedTE True --seed 12345 --cuda '' --print True --tune '' --normalize 3 

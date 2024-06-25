4213eb59efa30e1ec6e1e4dab8e7e3d377234503
janik96v <janik.vollenweider@gmail.com>
2024-06-25 13:50:38 +0200

    Minor changes

--- runATRS_warp.sh
+++ runATRS_warp.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python main.py --model TENet --window 8 --horizon 4 --batch_size 16 --channel_size 16 --hid1 30 --hid2 20 --lr 0.0011960721309568513 --epochs 1000 --data ./Data/form41_aggregated_quarterly_reduced_LargeUSC.csv --A ./TENet_master/TE/form41_aggregated_quarterly_USC_reduced_TE.txt --form41 True --airline_batching True --sharedTE True --seed 123 --cuda True --print True --tune True --tune_trials 30
+python main.py --model TENet --window 8 --horizon 4 --batch_size 16 --channel_size 16 --hid1 30 --hid2 20 --lr 0.0011960721309568513 --epochs 200 --data ./Data/form41_aggregated_quarterly_reduced_LargeUSC.csv --A ./TENet_master/TE/form41_aggregated_quarterly_USC_reduced_TE.txt --form41 True --airline_batching True --sharedTE True --seed 123 --cuda True --print True --tune True --tune_trials 30 --normalize 3 

acc259c4c54d931c56e9cdc45caec5e374fc0565
janik96v <janik.vollenweider@gmail.com>
2024-06-25 14:55:58 +0200

    Minor changes

--- runTE_calculation.sh
+++ runTE_calculation.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python ./TENet_master/util/Teoriginal.py --datapath ./Data/form41_aggregated_quarterly_pca6.csv --outputfolder ./TENet_master/TE --pca_processed True
+python ./TENet_master/util/Teoriginal.py --datapath ./Data/form41_aggregated_quarterly_reduced_LargeUSC.csv --outputfolder ./TENet_master/TE --pca_processed ''
